32064df713727f123c8359a1afe177c56c99dce6
ravi-prog <ravindra3885@gmail.com>
2024-01-26 01:28:28 +0530

    removed animations

--- home.py
+++ home.py
@@ -1,3 +1,4 @@
+import markdown
 import streamlit as st
 from streamlit_option_menu import option_menu
 from streamlit_lottie import st_lottie
@@ -8,7 +9,8 @@ def css_loader(url):
     with open(url,"r") as f:
         st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
 css_loader("styles/style.css")
-
+def empty():
+    return " "
 with st.container():
     selected=option_menu(
         menu_title=None,
@@ -20,35 +22,52 @@ if selected =="About":
     col1,col2=st.columns(2)
     with col1:
         st.write("##")
-        st.subheader("I am RAVINDRA PANDEY")
-        st.title("ML Engineer")
+        st.write("##")
+        st.write("##")
+        st.header("I am RAVINDRA PANDEY")
+        st.title("Data Science Postgraduate")
     with col2:
-        st_lottie(json.load(open("assests/animation_laptop.json","r")))
+        # st_lottie(json.load(open("assests/animation_laptop.json","r")))
+        pass
     st.write("---")
     col3,col4 = st.columns(2)
     with col3:
-        st.subheader("""
-                    Education
-                    - MCA Data Science
+        st.markdown(f""" 
+                    # Education
+                    ----------
+                    ### MCA Data Science
                         - Dev Sanskriti Vishwavidyalaya
+                        - 7.62 SGPA, 8.4 SGPA, 8.33 SGPA, _
                         - 2022 - pusuing
-                    - BSC Applied Mathematics
+                        
+                    ### BSC Applied Mathematics
                         - Dev Sanskriti Vishwavidyalaya
+                        - 7.2 CGPA
                         - 2019 - 2022
-                    - 12th
+                    ### 12th
                         - Inter Science College , Hazaribagh
+                        - 51.4%
                         - 2017 - 2019
-                    - 10th
+                    ### 10th
                         - St. Stephen School
+                        - 9.6 CGPA
                         - 2017
-                     """)      
+                     """ ,unsafe_allow_html=True)      
     with col4:
-        st.subheader("""
-                    Experiences
-                    - No experience in any company but gained experience from building projects which you can check out in projects section. 
-                    - lokking for oportunities .....
-                     """)
-        st_lottie(json.load(open("assests/animation_looking_ahead.json","r")))
+        st.markdown("""
+                    # Work Experiences
+                    -------------------
+                    ### Machine Learning Mentor
+                        - National Institute of Electronics and Information Technology, Haridwar
+                        - Training JNV students in Tehri Garhwal on Python and ML.
+                        - Increased student engagement & practical knowledge: hands-on coding, 
+                        interactive notebooks, project simulations (data analysis, prediction models).
+                        - Bridged academic-real world gap: dynamically tailored curriculum.
+                        - Expanding to NLP & Deep Learning modules.
+                    ### Center of Artificial Intelligence & Research
+                     """,unsafe_allow_html=True)
+        
+        # st_lottie(json.load(open("assests/animation_looking_ahead.json","r")))
         
 if selected=="Projects":
     st.balloons()
