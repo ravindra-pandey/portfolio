84cf56cc71ebdc75c6cb53c73234402762a8d00d
ravi-prog <ravindra3885@gmail.com>
2024-01-27 21:12:51 +0530

    started bulding projects section

--- projects.py
+++ projects.py
@@ -5,10 +5,6 @@ def list_projects():
     st.title("projects")
     col1,col2=st.columns(2)
     with col1:
-        st.markdown("""
-                    E-mail : ravindra.kr.py@gmail.com
-                    linkedin: 
-                    """,unsafe_allow_html=True)
+        pass
     with col2:
-        st_lottie(json.load(open("assests/contact.json", "r")),key="animation_container")
-    
+        pass
